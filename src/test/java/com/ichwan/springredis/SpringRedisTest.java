7b4c5736efa549ab749d60090745b18fdfc6b339
ichwansh03 <ichwansholihin03@gmail.com>
2024-02-12 17:06:46 +0700

    add SpringRedisTest (ops for execute and pipeline)

--- src/test/java/com/ichwan/springredis/SpringRedisTest.java
+++ src/test/java/com/ichwan/springredis/SpringRedisTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.geo.Circle;
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.GeoResults;
@@ -12,9 +13,11 @@ import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.domain.geo.Metrics;
 
+import java.util.List;
 import java.util.Set;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 @SpringBootTest
 public class SpringRedisTest {
@@ -84,5 +87,45 @@ public class SpringRedisTest {
         ));
 
         Assertions.assertEquals(2, searched.getContent().size());
+    }
+
+    /**
+     * melakukan database transaction, semua perubahan akan dicommit di akhir
+     */
+    @Test
+    void execute() {
+        redisTemplate.execute(new SessionCallback<>() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                operations.multi();
+
+                operations.opsForValue().set("ichwan","Test01");
+                operations.opsForValue().set("sholihin","Test03");
+
+                operations.exec();
+                return null;
+            }
+        });
+
+        Assertions.assertEquals("Test01", redisTemplate.opsForValue().get("ichwan"));
+    }
+
+    /**
+     * melakukan input data sekaligus tanpa menunggu balasan satu persatu
+     */
+    @Test
+    void executePipeline() {
+        List<Object> list = redisTemplate.executePipelined(new SessionCallback<>() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+
+                operations.opsForValue().set("test1", "Hello world");
+                operations.opsForValue().set("test2", "Halo dunia");
+                return null;
+            }
+        });
+
+        assertThat(list, hasSize(2));
+        assertThat(list, hasItems(true));
     }
 }
